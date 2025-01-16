f4c060ec5181121cdfdff5d79a0c60dd98fb75eb
Rajeev Ranjan <159820641+RajeevRanjansing@users.noreply.github.com>
2025-01-17 02:18:24 +0530

    style.js

--- style.js
+++ style.js
@@ -8,7 +8,7 @@ export default ExStyles=StyleSheet.create({
     },
   
     head:{
-      height:"40%",
+      height:"30%",
       width:"100%",
       backgroundColor:"#ff006e",
       display:"flex",
@@ -26,9 +26,33 @@ export default ExStyles=StyleSheet.create({
       color:"white",
       textAlignVertical:"center"
     },
+
+    container:{
+      display:"flex",
+      flexDirection:"row",
+      justifyContent:"space-evenly",
+      height:50,
+      marginTop:30,
+    },
+
+    btn:{
+      backgroundColor:"#2196F3",
+      width:"38%",
+      height:40,
+      borderRadius:8,
+      display:"flex",
+      justifyContent:"center",
+      alignItems:"center"
+
+    },
+    button:{
+      fontSize:25,
+      color:"white",
+      textAlign:"center",
+
+    },
   
-    textBox:{
-      
+    textBox:{  
     height:45,
     width:"80%",
     borderWidth:2,  
@@ -40,30 +64,6 @@ export default ExStyles=StyleSheet.create({
     
     },
   
-    container:{
-      flex:1,
-      alignItems:"center",
-      justifyContent:"center",
-    },
+
   
-    buttonContainer: {
-      marginTop:10,
-      flexDirection: 'row',
-      justifyContent: 'space-between', 
-      width: '75%', 
-      
-    },
-  
-    button: {
-      width: '40%',
-      backgroundColor: '#2196F3',
-      padding: 10,
-      borderRadius: 5,
-      alignItems: 'center', 
-    },
-  
-    buttonText: {
-      color: 'white',
-      fontSize: 16,
-    },
   })
